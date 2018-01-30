5e4ad0b8c99c2181dcdb49a5883e7d96b1fbfe29
Valentina Cupac <valentina.cupac@optivem.com>
2018-01-30 22:10:41 +0100

    Added the subtractor

---
+++ subtractor/src/subtractor/Main.java
@@ -0,0 +1,25 @@
+package subtractor;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try(Scanner scanner = new Scanner(System.in)){
+			
+			System.out.println("Input the first number: ");
+			int firstNumber = scanner.nextInt();
+			
+			System.out.println("Input the second number: ");
+			int secondNumber = scanner.nextInt();
+			
+			System.out.println("System is now doing the calculation...");
+			int difference = firstNumber - secondNumber;
+			
+			System.out.println("The difference between " + firstNumber + " and " + secondNumber + " is: " + difference);	
+		}
+
+	}
+
+}
