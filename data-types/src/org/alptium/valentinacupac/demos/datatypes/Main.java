94462ee99cb781401fe03e04d2f1f5b3affc5581
Valentina Cupac <valentina.cupac@optivem.com>
2018-01-31 23:24:52 +0100

    Added data types

---
+++ data-types/src/org/alptium/valentinacupac/demos/datatypes/Main.java
@@ -0,0 +1,59 @@
+package org.alptium.valentinacupac.demos.datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Basic print statements
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Apple!");
+		System.out.println(5);
+		System.out.println(4 + 3);
+		System.out.println(true);
+		System.out.println(3509.23);
+		System.out.println("Account balance: " + 3509.74);
+		
+		// Primitive data types
+		
+		// Integer (int)
+		int ab = 14;
+		int lala = 150;
+		int zebra = 234;
+		int luk = 45;
+		
+		System.out.println("Value of ab: " + ab);
+		
+		// Character (char)
+		char jkl = 'a';
+		char gh = '8';
+		char sfgs = '$';
+		
+		System.out.println("Value of sfgs: " + sfgs);
+		
+		// Boolean (bool)
+		boolean yt = true;
+		boolean lop = false;
+		boolean df = true;
+		
+		System.out.println("Value of df: " + df);
+		
+		// Double 
+		
+		double ujl = 45.89;
+		double er = 786579.982;
+		double vb = 0.06864;
+		
+		System.out.println("Value of er: " + er);
+		
+		// Composite data types
+		
+		// String (text, composed of characters)
+		String jds = "Marija Marjanovic";
+		String ywe = "Ulica 5, Beograd";
+		String ulo = "Fly to the moon";
+		
+		System.out.println("Value of er: " + jds);
+	}
+
+}
