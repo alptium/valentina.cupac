31141db045f9efe705f47f43c02a86a2243dc3fe
Valentina Cupac <valentina.cupac@optivem.com>
2018-03-01 21:19:45 +0100

    Added code conventions sample project

---
+++ code-conventions/src/codeconventions/Main.java
@@ -0,0 +1,48 @@
+package codeconventions;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 6;
+		int z = 90;
+		
+		int sum = x + y + z;
+		
+		System.out.println(sum);
+		
+		try(Scanner sc = new Scanner(System.in)) {
+			int lala = 56;
+			int hello = 87;
+			
+			for(int i = 0; i < 10; i++) {
+				int fdsf = 32423;
+				int dsf = 455;
+				
+				if(lala == 56) {
+					int hs = 343;
+					int me = 34;
+				}
+				
+				if(dsf == 782) {
+					System.out.println("Something here...");
+				}
+				
+			}
+			
+		}
+		
+		for(int i = 0; i < 56; i++) {
+			int dfsfd = 32532;
+			int dhd = 234;
+			
+			System.out.println("Something else");
+		}
+		
+		
+	}
+
+}
